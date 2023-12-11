c23d1e12ad736bb6501bc31eb5f32a566c3c6f02
Dan Muchiri <dan.muchiri@student.moringaschool.com>
2023-12-11 11:25:30 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+let employee = {name: 'Dan', streetAddress: 'Imara'}
+function updateEmployeeWithKeyAndValue(employee, key, value){
+    const newObj = { ...employee };
+  newObj[key] = value;
+  return newObj;
+  return employee;
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value;
+    return employee;
+}
+function deleteFromEmployeeByKey(employee, key){
+    const deletedObj = { ...employee };
+  delete deletedObj[key];
+  return deletedObj;
+  return employee;
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+  delete employee[key];
+  return employee;
+}
